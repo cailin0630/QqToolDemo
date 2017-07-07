6971792b2ef95859bb3acc76a586f7a04e2e432e
alvinjiang <alvinjiang@yuantutech.com>
2017-07-07 22:44:11 +0800

    import code

---
+++ QQDemo/QqInfoProvide.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Automation;
+
+namespace QQDemo
+{
+    public class QqInfoProvide
+    {
+        public static List<QqInfo> GetCurrentInfo()
+        {
+            var list = new List<QqInfo>();
+            var process = Process.GetProcessesByName("QQ");
+            if (process.Length == 0)
+                return list;
+            //获取根节点
+            var aeTop = AutomationElement.RootElement;
+
+            //查找窗体名
+            var aeForm = aeTop.FindAll(TreeScope.Children, new PropertyCondition(AutomationElement.IsEnabledProperty, true));
+
+            foreach (AutomationElement ae in aeForm)
+            {
+                if (ae.Current.ClassName == "TXGuiFoundation" && ae.Current.AccessKey != "")
+                    list.Add(new QqInfo
+                    {
+                        Name = ae.Current.Name
+                    });
+            }
+            return list;
+        }
+
+        public class QqInfo
+        {
+            public string Name { get; set; }
+        }
+    }
+}
